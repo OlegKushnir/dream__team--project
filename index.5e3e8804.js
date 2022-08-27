62b2dd5c5617ee66a6eec4da0a4f03ab2443b621
OlegKushnir <108745741+OlegKushnir@users.noreply.github.com>
2022-08-27 21:03:53 +0000

    Deploying to gh-pages from @ OlegKushnir/dream__team--project@20f76a23a2f5f1526c897c7a3f9e01191f727ded 🚀

---
+++ index.5e3e8804.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".galary__slide").slick({autoplay:!0,infinite:!0,speed:700,fade:!0,cssEase:"linear"})})),$(document).ready((function(){$(".feedback__slide").slick({autoplay:!0,dots:!0,infinite:!0,speed:2e3,slidesToShow:1})}));
+//# sourceMappingURL=index.5e3e8804.js.map
